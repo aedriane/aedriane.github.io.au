fc23939ca8ba83c07651e2a2db3c2dd7b28be4b8
aedriane <aedrianehernan@gmail.com>
2023-12-05 12:32:08 +1100

    user.js cahnges

--- src/data/user.js
+++ src/data/user.js
@@ -18,7 +18,7 @@ const INFO = {
 	homepage: {
 		title: "Cloud and Systems Engineer.",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a Cloud and Systems Engineer with extensive experience with various technologies. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
